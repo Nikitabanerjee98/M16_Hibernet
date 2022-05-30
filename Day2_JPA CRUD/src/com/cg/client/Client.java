c770b3608b7669e29645c58d3f9c3dad134ffbd0
Nikita <Nikita@DESKTOP-ERTFKKH>
2022-05-30 23:13:31 +0530

    second commit

---
+++ Day2_JPA CRUD/src/com/cg/client/Client.java
@@ -0,0 +1,37 @@
+package com.cg.client;
+
+import com.cg.entities.Student;
+import com.cg.service.StudentService;
+import com.cg.service.StudentServiceImpl;
+
+public class Client {
+
+	public static void main(String[] args) {
+		StudentService service=new StudentServiceImpl();
+		Student student=new Student();
+		//CRUD Operation
+		//create
+		student.setID(24);
+		student.setName("Akansha");
+		service.addStudent(student);
+		
+		/*//Retreive data-find();
+		student=service.findStudentById(21);
+		System.out.println("ID: "+student.getID());
+		System.out.println("ID: "+student.getName());*/
+		
+		
+		/*//update
+		student=service.findStudentById(21);
+		student.setName("Nikitha");
+		service.updateStudent(student);*/
+		
+		//delete
+		student=service.findStudentById(21);
+		service.removeStudent(student);
+		
+		
+
+	}
+
+}
---
+++ Day2_JPA CRUD/src/com/cg/service/StudentService.java
@@ -0,0 +1,12 @@
+package com.cg.service;
+
+import com.cg.entities.Student;
+
+public interface StudentService 
+{
+	abstract void addStudent(Student student);
+	abstract Student findStudentById(int ID);
+	abstract void updateStudent(Student student);
+	abstract void removeStudent(Student student);
+
+}
